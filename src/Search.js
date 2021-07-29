b8d83099311b4bd16cc4cbd36f859c5f8a94d81a
Jordan Renaud <jordan.dj.renaud@gmail.com>
2021-07-29 10:20:15 +0100

    add logic to add new threads on search patterns page

--- src/Search.js
+++ src/Search.js
@@ -1,10 +1,47 @@
 import "./Search.css";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import queryString from "query-string";
 
 export default function Search() {
+  const [thread, setThread] = useState("");
+  const history = useHistory();
+
+  function handleClick(event) {
+    event.preventDefault();
+    sendThread();
+  }
+
+  function getThread(e) {
+    setThread(e.target.value);
+  }
+
+  function handleKeypress(e) {
+    if (e.key === "Enter") {
+      sendThread();
+    }
+  }
+
+  function sendThread() {
+    history.push(
+      `/search-patterns?${queryString.stringify({
+        thread: thread.trim(),
+      })}`
+    );
+  }
+
   return (
     <div className="Search">
-      <input type="search" id="thread-search" placeholder="Search threads..." />
-      <button id="search-button">Search</button>
+      <input
+        type="search"
+        id="thread-search"
+        placeholder="Search threads..."
+        onChange={getThread}
+        onKeyPress={handleKeypress}
+      />
+      <button id="search-button" onClick={handleClick}>
+        Search
+      </button>
     </div>
   );
 }
--- src/SearchPatterns.js
+++ src/SearchPatterns.js
@@ -1,19 +1,39 @@
 import Search from "./Search";
 import "./SearchPatterns.css";
+import { threadData } from "./data/threadData";
 import Thread from "./Thread.js";
+import { useLocation } from "react-router-dom";
+import queryString from "query-string";
+import { useState, useEffect } from "react";
 
 export default function SearchPatterns() {
+  const location = useLocation();
+  let newThread = queryString.parse(location.search).thread;
+  const [threads, setThreads] = useState([]);
+
+  useEffect(() => {
+    threadData.forEach((thread) => {
+      if (thread.number === newThread) {
+        console.log("found");
+        setThreads([...threads, { name: newThread, amount: 1 }]);
+      }
+    });
+  }, [newThread]);
+
+  console.log(threads);
+
   return (
     <div className="SearchPatterns">
+      <div>
+        <p>Please enter a valid DMC thread</p>
+      </div>
       <div className="search-container">
         <Search />
       </div>
       <div className="flex-container">
-        <Thread threadNumber="310" />
-        <Thread threadNumber="3713" />
-        <Thread threadNumber="3755" />
-        <Thread threadNumber="702" />
-        <Thread threadNumber="445" />
+        {threads.map((thread) => (
+          <Thread threadNumber={thread.name} />
+        ))}
       </div>
     </div>
   );
