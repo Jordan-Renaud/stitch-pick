41f77395e704985bcad5cc0f0ac0f9a82091de74
Jordan Renaud <jordan.dj.renaud@gmail.com>
2021-08-02 14:40:41 +0100

    add styling to thread component to make smaller

--- src/SearchPatterns.js
+++ src/SearchPatterns.js
@@ -11,10 +11,29 @@ export default function SearchPatterns() {
   let newThread = queryString.parse(location.search).thread;
   const [threads, setThreads] = useState([]);
 
+  let testThreads = [
+    { name: "800", amount: 1 },
+    { name: "801", amount: 1 },
+    { name: "803", amount: 1 },
+    { name: "806", amount: 1 },
+    { name: "807", amount: 1 },
+    { name: "809", amount: 1 },
+    { name: "813", amount: 1 },
+    { name: "814", amount: 1 },
+    { name: "815", amount: 1 },
+    { name: "816", amount: 1 },
+    { name: "817", amount: 1 },
+    { name: "818", amount: 1 },
+    { name: "819", amount: 1 },
+    { name: "820", amount: 1 },
+    { name: "800", amount: 1 },
+  ];
+
   useEffect(() => {
     threadData.forEach((thread) => {
       if (thread.number === newThread) {
         console.log("found");
+
         setThreads([...threads, { name: newThread, amount: 1 }]);
       }
     });
@@ -24,15 +43,17 @@ export default function SearchPatterns() {
 
   return (
     <div className="SearchPatterns">
-      <div>
+      <div className="thread-error">
         <p>Please enter a valid DMC thread</p>
       </div>
       <div className="search-container">
         <Search />
       </div>
       <div className="flex-container">
-        {threads.map((thread) => (
-          <Thread threadNumber={thread.name} />
+        {threads.map((thread, index) => (
+          <div className={`box${index + 1}`}>
+            <Thread threadNumber={thread.name} />
+          </div>
         ))}
       </div>
     </div>
